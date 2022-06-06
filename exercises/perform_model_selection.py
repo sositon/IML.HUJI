d320ff1562be33dd61304be7700edf4048a4251a
Omer Siton <omersiton11@gmail.com>
2022-06-06 16:47:55 +0300

    first two graphs

--- exercises/perform_model_selection.py
+++ exercises/perform_model_selection.py
@@ -5,7 +5,8 @@ from sklearn import datasets
 from IMLearn.metrics import mean_square_error
 from IMLearn.utils import split_train_test
 from IMLearn.model_selection import cross_validate
-from IMLearn.learners.regressors import PolynomialFitting, LinearRegression, RidgeRegression
+from IMLearn.learners.regressors import PolynomialFitting, LinearRegression, \
+    RidgeRegression
 from sklearn.linear_model import Lasso
 
 from utils import *
@@ -27,16 +28,56 @@ def select_polynomial_degree(n_samples: int = 100, noise: float = 5):
     """
     # Question 1 - Generate dataset for model f(x)=(x+3)(x+2)(x+1)(x-1)(x-2) + eps for eps Gaussian noise
     # and split into training- and testing portions
-    raise NotImplementedError()
+
+    # generate the data samples
+    X = np.linspace(-2, 2, n_samples)
+    e = np.random.normal(0, noise, n_samples)
+    f = lambda x: (x + 3)*(x + 2)*(x + 1)*(x - 1)*(x - 2)
+    y = f(X) + e
+    train_portion = 2/3
+    X_train, y_train, X_test, y_test = split_train_test(pd.DataFrame(X), pd.Series(y), train_portion)
+
+    fig = go.Figure()
+    # Add traces
+    fig.add_trace(go.Scatter(x=X, y=f(X),
+                             mode='lines',
+                             name='Noiseless Model',
+                             showlegend=True))
+    fig.add_trace(go.Scatter(x=X, y=np.array(y_train),
+                             mode='markers',
+                             name='Train',
+                             showlegend=True))
+    fig.add_trace(go.Scatter(x=X, y=np.array(y_test),
+                             mode='markers',
+                             name='Test',
+                             showlegend=True))
+    # fig.show()
 
     # Question 2 - Perform CV for polynomial fitting with degrees 0,1,...,10
-    raise NotImplementedError()
+    max_degree = 11
+    k_train_scores, k_valid_scores = [], []
+    for k in range(max_degree):
+        ts, vs = cross_validate(PolynomialFitting(k), np.array(X_train).reshape(-1), np.array(y_train).reshape(-1), mean_square_error)
+        k_train_scores.append(ts)
+        k_valid_scores.append(vs)
+
+    fig = go.Figure()
+    # Add traces
+    fig.add_trace(go.Scatter(x=list(range(max_degree)), y=k_train_scores,
+                             mode='lines',
+                             name='Train Scores',
+                             showlegend=True))
+    fig.add_trace(go.Scatter(x=list(range(max_degree)), y=k_valid_scores,
+                             mode='lines',
+                             name='Validation Scores',
+                             showlegend=True))
+    fig.show()
 
     # Question 3 - Using best value of k, fit a k-degree polynomial model and report test error
-    raise NotImplementedError()
 
 
-def select_regularization_parameter(n_samples: int = 50, n_evaluations: int = 500):
+def select_regularization_parameter(n_samples: int = 50,
+                                    n_evaluations: int = 500):
     """
     Using sklearn's diabetes dataset use cross-validation to select the best fitting regularization parameter
     values for Ridge and Lasso regressions
@@ -61,4 +102,4 @@ def select_regularization_parameter(n_samples: int = 50, n_evaluations: int = 50
 
 if __name__ == '__main__':
     np.random.seed(0)
-    raise NotImplementedError()
+    select_polynomial_degree()
